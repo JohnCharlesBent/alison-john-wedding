aafbdde9c0e82925a06368d90770aa21e4d0a9c0
JohnCharlesBent <johnbentdesign@gmail.com>
2019-06-09 21:21:49 -0400

    pushing up the footer

--- inc/footer.php
+++ inc/footer.php
@@ -4,7 +4,7 @@
 **/
 ?>
   <footer>
-
+    <h2 class="gothic">We hope you'll join us!</h2>
   </footer>
 
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
--- inc/pvd.php
+++ inc/pvd.php
@@ -2,14 +2,19 @@
 // -- about PVD
 ?>
 
-<div class="column_one">
+<div class="full-width">
+  <h2 class="gothic">Enjoy Beautiful Providence!</h2>
+  <p>Providence is bursting with great restaurants, bars, art galleries, and record stores. Enjoy some of the many wonderful things about Providence while you're here!</p>
+</div>
 
+<div class="column_one">
+  <h3>Accomodations</h3>
 </div>
 
 <div class="column_two">
-
+  <h3>Food</h3>
 </div>
 
 <div class="column_three">
-
+  <h3>Fun</h3>
 </div>
